9eeb6c5275514f3d71c1400d0f0c4adaeba834de
VxTrustedTyler <tylergayle16@gmail.com>
2025-08-29 12:58:47 -0400

    Delete Perkify Tickets directory

--- Perkify Tickets/addons/example/example.js
+++
@@ -1,5 +0,0 @@
-module.exports.run = async (client) => {
-    client.on('ready', async (client) => {
-        console.log("This is an example addon! You can delete it in addons/example")
-    });
-};
